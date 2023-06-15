a7778deedb2c10b574b7ce912a96b4b7c5f66ebe
Emmanuel Koech <emmanuelkoech1820@gmail.com>
2023-06-15 08:04:07 +0300

    Add project files.

---
+++ Employee.Test/EmployeeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using NUnit.Framework;
+
+[TestFixture]
+public class EmployeeDALTests
+{
+    private MySqlConnection connection;
+    private EmployeeDAL dal;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Set up the connection to the test database
+        string connectionString = "your_test_mysql_connection_string";
+        connection = new MySqlConnection(connectionString);
+        connection.Open();
+
+        // Initialize the data access layer
+        dal = new EmployeeDAL(connection);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Clean up the test database
+        connection.Close();
+    }
+
+    [Test]
+    public void TestUpdateEmployeeSalary()
+    {
+        // Arrange
+        int employeeId = 1;
+        int newSalary = 50000;
+        int oldSalary;
+
+        // Act
+        oldSalary = dal.UpdateEmployeeSalary(employeeId, newSalary);
+
+        // Assert
+        Assert.AreEqual(40000, oldSalary);
+    }
+}
---
+++ UpdateSalaryScript.sql
@@ -0,0 +1,14 @@
+﻿
+
+CREATE PROCEDURE UpdateEmployeeSalary(
+    IN employeeId INT,
+    IN newSalary INT,
+    OUT oldSalary INT
+)
+BEGIN
+    -- Store the old salary
+    SELECT salary INTO oldSalary FROM employees WHERE id = employeeId;
+
+    -- Update the salary
+    UPDATE employees SET salary = newSalary WHERE id = employeeId;
+END 
